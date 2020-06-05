d3164d6603f9ca9b02a5579f97d72d0ec32a2422
Callum Cox <callumcox2412@gmail.com>
2020-06-05 09:49:16 +0100

    Updated import

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-import App from "./App";
+import App from ".";
 
 describe("<App>", () => {
   it("renders Hello there!", () => {
